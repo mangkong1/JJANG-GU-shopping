c569758123b74bfee70e3fccd7bb8431e02d1a5e
kojeounhwa <rhwidghk125@naver.com>
2023-11-03 10:42:52 +0900

    Design

--- pages/main.js
+++ pages/main.js
@@ -8,8 +8,8 @@ document.querySelector('#headerWrap').innerHTML = `
       </a></h1>
       <ul class="login">
         <li class="inline-block"><a href="#">로그인</a></li>
-        <li class="inline-block"><a href="#">회원가입</a></li>
-        <li class="inline-block relative">
+        <li class="inline-block pl-10"><a href="#">회원가입</a></li>
+        <li class="inline-block relative pl-10">
           <a href="#">
             <i class="fa-solid fa-cart-shopping text-lg"></i>
             <span class="block w-4 h-4 bg-red rounded-full absolute top-0 right-[-10px]" ></span>
@@ -20,23 +20,23 @@ document.querySelector('#headerWrap').innerHTML = `
     `
 
 document.querySelector('#footerWrap').innerHTML=`
-<div id="footer" class="w-full flex-col flex justify-center items-center relative">
+<div id="footer" class="w-full flex-col flex justify-center items-center border-t border-gray400 h-72 relative">
 
-      <div class="Scroll_top cursor-pointer">
-        <i class="fa-solid fa-arrow-up"></i>
+      <div class="Scroll_top absolute top-10 right-10 w-12 h-12 rounded-full border border-gray500 flex justify-center items-center cursor-pointer">
+        <i class="fa-solid fa-arrow-up text-xl test-gray500"></i>
       </div>
 
       <ul class="link_list pb-10">
         <h3 class="hidden">떡잎마을 상점 정책 및 약관</h3>
-        <li class="link_item inline-block"><a href="#" class="text-gray500">이용약관</a>|</li>
-        <li class="link_item inline-block"><a href="#" class="text-gray500">전자금융거래 이용약관</a>|</li>
-        <li class="link_item inline-block"><a href="#" class="text-gray500">개인정보처리방침</a>|</li>
-        <li class="link_item inline-block"><a href="#" class="text-gray500">안전거래 가이드</a>|</li>
-        <li class="link_item inline-block"><a href="#" class="text-gray500">고객센터</a>|</li>
+        <li class="link_item inline-block px-3 border-r-2 border-gray400"><a href="#" class="text-gray500">이용약관</a></li>
+        <li class="link_item inline-block px-3 border-r-2 border-gray400"><a href="#" class="text-gray500">전자금융거래 이용약관</a></li>
+        <li class="link_item inline-block px-3 border-r-2 border-gray400"><a href="#" class="text-gray500">개인정보처리방침</a></li>
+        <li class="link_item inline-block px-3 border-r-2 border-gray400"><a href="#" class="text-gray500">안전거래 가이드</a></li>
+        <li class="link_item inline-block px-3"><a href="#" class="text-gray500">고객센터</a></li>
       </ul>
       <div class="link_address">
         <h3 class="hidden">떡잎마을 상점</h3>
-        <a href="#" class="inline-block text-gray400 text-sm"><img class="inline-block" src="./imgs/f-logo.svg" alt="떡잎마을 상점 로고">Copyright © 2023. 떡잎마을 상점 All Rights Reserved.</a>
+        <a href="#" class="inline-block text-gray400 text-sm"><img class="inline-block pr-3 h-6" src="./imgs/f-logo.svg" alt="">Copyright © 2023. 떡잎마을 상점 All Rights Reserved.</a>
       </div>
     </div>`
 
