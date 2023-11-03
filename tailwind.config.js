fd022c62fbc5b3fd0bb9fa1a3d7392df054e1a64
[SW7] 박소영 <soyo19585@gmail.com>
2023-11-03 14:44:06 +0900

    🐛Fix: show style

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-      './pages/**/*.html'
+      './pages/**/*.{html,js}',
   ],
   theme: {
     extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'black': '#000000',
+      'white90': '#fff9',
+      'gray200': '#cfcfcf',
+      'gray400': '#8B8B8B',
+      'gray500': '#545454',
+      'red': '#F13737',
+      'test': '#97caf6',
+      'test1': '#fff7c8',
+    },
+    backgroundImage: {
+      'banner_1': "url('./public/imgs/banner_1.svg')",
+      'banner_2': "url('./public/imgs/banner_2.svg')",
+    }
   },
   plugins: [],
 }
-
