5641ff8800a5ad3df97759517728bdcfbe344f80
[SW7] 김태형 <buzzkim53@gmail.com>
2023-11-05 00:58:47 +0900

    Rename: add order-finish folder

--- pages/Detail/detail.js
+++ pages/Detail/detail.js
@@ -1,1 +1,14 @@
 import "../../index.css";
+
+function addCart() {
+  var currentUrl = window.location.href;
+  var newUrl = currentUrl.replace("/detail/", "/cart/");
+  window.location.href = newUrl;
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  var addCartButton = document.querySelector(".Add_cart");
+  if (addCartButton) {
+    addCartButton.addEventListener("click", addCart);
+  }
+});
