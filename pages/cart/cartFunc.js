5a38239f289be33caf031b7456a6b91d508b8c0b
[SW7] 박소영 <soyo19585@gmail.com>
2023-11-04 14:36:28 +0900

    📝Docs: work on cartFunc (incomplete!!)

--- pages/cart/cartFunc.js
+++ pages/cart/cartFunc.js
@@ -2,11 +2,11 @@ import { addItem, removeItem, removeAllItem, getTotalPrice, updateQuantity, getT
 
 const deleteAllBtn = document.getElementById("deleteAllBtn");
 const deleteSelectedBtn = document.getElementById("deleteSelectedBtn");
-const totalItemNum = document.getElementById("totalItemNumber");
+const totalAmount = document.getElementById("totalAmount");
 const totalItemPrice = document.getElementById("totalItemPrice");
 const shipPrice = document.getElementById("shipPrice");
 const totalPrice = document.getElementById("totalPrice");
-const cartItem = document.getElementById("cartItems");
+const cartItem = document.getElementById("cartItem");
 
 fetch("./items.json")
   .then((res) => {
@@ -22,34 +22,42 @@ fetch("./items.json")
 let items = JSON.parse(localStorage.getItem("items"));
 let cart = JSON.parse(localStorage.getItem("cart"));
 
-function addItemtoCart() {
+function showIteminCart() {
   cartItem.innerHTML = cart
     .map((item, i) => {
-      ` <div class="flex items-center">
-        <input checked id="${item.id}" type="checkbox" class="w-[10%] h-[10%] text-gray200 border-gray200">
-            <div class="h-[40%] w-[40%] m-2 mt-4">
-            <img class="itemImg 2xl object-fit rounded-2xl" src="${item.image}" alt="cartImg">
-            </div>
-        <label for="${item.id}" class="mx-2">${item.name}</label>
-    </div>
-    <div class="flex justify-center items-center">
-        <input type="number" value="${item.quantity}" min="1" class="itemNumber w-[2.5rem] h-[1.3rem] border border-solid border-gray200 rounded-sm"/>
-    </div>
-        <p class="flex justify-center items-center font-bold ml-2">${item.price}</p>`;
+      ` <div id="cartItem-${item.id}" class="grid grid-cols-3 grid-cols-[4fr_1fr_2fr]  justify-items-center items-center"> 
+          <div class="flex items-center">
+            <input checked id="check-${item.id}" type="checkbox" class="w-[10%] h-[10%] text-gray200 border-gray200">
+                <div class="h-[40%] w-[40%] m-2 mt-4">
+                <img class="itemImg 2xl object-fit rounded-2xl" src="${item.image}" alt="cartImg">
+                </div>
+            <label for="check-${item.id}" class="mx-2">${item.name}</label>
+          </div>
+          <div class="flex justify-center items-center">
+            <input type="number" value="${item.quantity}" min="1" max="99" class="itemNumber w-[2.5rem] h-[1.3rem] border border-solid border-gray200 rounded-sm"/>
+          </div>
+          <p class="flex justify-center items-center font-bold ml-2">${item.price}</p>
+        </div>`;
     })
     .join("");
 }
 
-const parentElement = document.querySelector(".a");
-removeElements(parentElement);
-
-function removeChosenItem() {
-  const checkbox = document.querySelectorAll('input[type="checkbox"]');
-  checkbox.forEach((item) => {
-    if (item.checked) {
-      checkbox.parentElementremove("");
-    }
-  });
+function showPayInfo() {
+  totalAmount.innerHTML = getTotalAmount();
+  totalItemPrice.inner;
 }
 
-addItemtoCart();
+function getitemId(itemId) {
+  const splitId = itemId.split("-");
+  return splitId[1];
+}
+
+function removeChosenItem() {
+  const checkedBoxes = document.querySelectorAll('input[type="checkbox"]:checked');
+  checkedBoxes.forEach((checkedBox) => {
+    const itemId = getitemId(checkedBox.id);
+    const cartItem = document.getElementById(`cartItem-${idNumber}`);
+    cartItem.remove();
+    removeItem(itemId);
+  });
+}
