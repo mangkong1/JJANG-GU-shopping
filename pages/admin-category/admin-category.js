dca6e89706c4b6d70a4956cea45de87694da1074
kojeounhwa <rhwidghk125@naver.com>
2023-11-07 00:33:04 +0900

    Feat: adminList

--- pages/admin-category/admin-category.js
+++ pages/admin-category/admin-category.js
@@ -32,3 +32,43 @@ categoryDelBtn.addEventListener("click", () => {
 categoryUpdateBtn.addEventListener("click", () => {
   confirm("카테고리를 수정하시겠습니까?");
 });
+
+document.querySelector("#adminList").innerHTML = `<div id="adminPageLeft">
+<h2 class="text-4xl font-semibold pb-6">관리자 페이지</h2>
+<div
+  class="userInfoList w-[236px] h-[528px] border border-gray200 rounded-2xl px-[25px] py-[36px] box-border"
+>
+  <ul>
+    <li class="font-semibold text-2xl mb-[87px]">
+      상품
+      <ul class="mt-[10px]">
+        <li>
+          <a class="text-xl font-normal text-gray400" href=""
+            >상품 관리</a
+          >
+        </li>
+      </ul>
+    </li>
+    <li class="font-semibold text-2xl mb-[87px]">
+      배송
+      <ul class="mt-[10px]">
+        <li>
+          <a class="text-xl font-normal text-gray400" href=""
+            >배송 관리</a
+          >
+        </li>
+      </ul>
+    </li>
+    <li class="font-semibold text-2xl">
+      카테고리
+      <ul class="mt-[10px]">
+        <li>
+          <a class="text-xl font-normal text-gray400" href=""
+            >카테고리 관리</a
+          >
+        </li>
+      </ul>
+    </li>
+  </ul>
+</div>
+</div>`;
