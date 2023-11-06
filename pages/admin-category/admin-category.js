7d43802058a49e9dc265fd5d38901158f5e8cd3e
kojeounhwa <rhwidghk125@naver.com>
2023-11-06 21:08:55 +0900

    Add: event handler

--- pages/admin-category/admin-category.js
+++ pages/admin-category/admin-category.js
@@ -1,11 +1,25 @@
 import "../../index.css";
 
+// 추가, 수정, 삭제 버튼 클릭 시
 const categoryUpdateBtn = document.querySelector(".categoryUpdateBtn");
 const categoryDelBtn = document.querySelector(".categoryDelBtn");
 const categoryAddBtn = document.querySelector(".categoryAddBtn");
 
 categoryAddBtn.addEventListener("click", () => {
-  alert("카테고리가 추가되었습니다.");
+  const categoryNum = document.querySelector(".categoryNum");
+  const categoryName = document.querySelector(".categoryName");
+
+  if (categoryNum.value === "") {
+    alert("번호를 입력해주세요.");
+  }
+
+  if (categoryName.value === "") {
+    alert("카테고리명을 입력해주세요.");
+  }
+
+  if (categoryNum.value !== "" && categoryName.value !== "") {
+    alert("카테고리가 추가되었습니다.");
+  }
 });
 
 categoryDelBtn.addEventListener("click", () => {
