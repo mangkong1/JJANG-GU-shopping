f3ee13cabc64fff74c09ced476b1a90488d1c68a
kojeounhwa <rhwidghk125@naver.com>
2023-11-06 17:16:24 +0900

    Add: content and event

--- pages/admin-category/admin-category.js
+++ pages/admin-category/admin-category.js
@@ -1,1 +1,17 @@
 import "../../index.css";
+
+const categoryUpdateBtn = document.querySelector(".categoryUpdateBtn");
+const categoryDelBtn = document.querySelector(".categoryDelBtn");
+const categoryAddBtn = document.querySelector(".categoryAddBtn");
+
+categoryAddBtn.addEventListener("click", () => {
+  alert("카테고리가 추가되었습니다.");
+});
+
+categoryDelBtn.addEventListener("click", () => {
+  confirm("카테고리를 삭제하시겠습니까?");
+});
+
+categoryUpdateBtn.addEventListener("click", () => {
+  confirm("카테고리를 수정하시겠습니까?");
+});
