722d0e09e5f5dd70e52f8af80f12c6d2fbf53de1
kojeounhwa <rhwidghk125@naver.com>
2023-11-08 01:12:11 +0900

    Design:

--- pages/admin-category/admin-category.js
+++ pages/admin-category/admin-category.js
@@ -1,51 +1,5 @@
 import "../../index.css";
 
-// 관리자페이지
-document.querySelector("#adminList").innerHTML = `<div id="adminPageLeft">
-<h2 class="text-4xl font-semibold pb-6">관리자 페이지</h2>
-<div
-  class="userInfoList w-[236px] h-[528px] border border-gray200 rounded-2xl px-[25px] py-[36px] box-border"
->
-  <ul>
-    <li class="font-semibold text-2xl mb-[87px]">
-      상품
-      <ul class="mt-[10px]">
-        <li>
-          <a class="text-xl font-normal text-gray400" href=""
-            >상품 목록</a
-          >
-        </li>
-        <li>
-          <a class="text-xl font-normal text-gray400" href=""
-            >상품 추가</a
-          >
-        </li>
-      </ul>
-    </li>
-    <li class="font-semibold text-2xl mb-[87px]">
-      배송
-      <ul class="mt-[10px]">
-        <li>
-          <a class="text-xl font-normal text-gray400" href=""
-            >배송 관리</a
-          >
-        </li>
-      </ul>
-    </li>
-    <li class="font-semibold text-2xl">
-      카테고리
-      <ul class="mt-[10px]">
-        <li>
-          <a class="text-xl font-normal text-gray400" href=""
-            >카테고리 관리</a
-          >
-        </li>
-      </ul>
-    </li>
-  </ul>
-</div>
-</div>`;
-
 const categoryList = document.querySelector(".categoryList");
 const changeCategoryList = (category) => {
   return `<li class="border-b border-gray200 grid-cols-3 py-4">
