dec62ed5ff21dafc4af565dcbca3f4d93886132b
kojeounhwa <rhwidghk125@naver.com>
2023-11-07 23:34:11 +0900

    Add: addBtn, deleteBtn event

--- pages/admin-category/admin-category.js
+++ pages/admin-category/admin-category.js
@@ -54,12 +54,12 @@ const changeCategoryList = (category) => {
     value="${category.name}"
     class="text-xl w-[500px] mr-[52px] py-2 rounded-2xl"
   />
-  <button
+  <button 
     class="categoryUpdateBtn w-[98px] h-[38px] bg-white rounded-[50px] border border-black mr-4 hover:border-red hover:text-red"
   >
     수정
   </button>
-  <button
+  <button 
     class="categoryDelBtn w-[98px] h-[38px] bg-white rounded-[50px] border border-black hover:border-red hover:text-red"
   >
     삭제
@@ -76,33 +76,83 @@ fetch("../category.json")
       const categoryLi = changeCategoryList(category);
       categoryList.innerHTML += categoryLi;
     });
+
+    addCategory(categoryList);
+    updateCategory();
+    deleteCategory(categoryList);
   })
   .catch((err) => {
     alert(`에러 : ${err}`);
   });
 
-// 추가, 수정, 삭제 버튼 클릭 시
-function btnClicked() {
-  const categoryUpdateBtn = document.querySelector(".categoryUpdateBtn");
+// 카테고리 수정
+function updateCategory() {
+  const categoryUpdateBtn = document.querySelectorAll(".categoryUpdateBtn");
+
+  categoryUpdateBtn.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      e.preventDefault();
+
+      alert("카테고리를 수정되었습니다.");
+    });
+  });
+}
+
+// 카테고리 삭제
+function deleteCategory() {
+  const categoryDelBtn = document.querySelectorAll(".categoryDelBtn");
+
+  categoryDelBtn.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      e.preventDefault();
+
+      const targetParentEl = e.target.parentElement;
+      categoryList.removeChild(targetParentEl);
+
+      alert("카테고리를 삭제되었습니다.");
+    });
+  });
+}
+
+// 카테고리 추가
+function addCategory(categoryList) {
   const categoryAddBtn = document.querySelector(".categoryAddBtn");
 
   categoryAddBtn.addEventListener("click", (e) => {
     e.preventDefault();
 
-    const categoryName = document.querySelector(".categoryName");
+    const inputEl = e.target.previousElementSibling;
+    const inputElValue = inputEl.value;
 
-    if (categoryName.value === "") {
+    if (inputElValue === "") {
       alert("카테고리명을 입력해주세요.");
     } else {
+      const categoryLi = `<li class="border-b border-gray200 grid-cols-3 py-4">
+        <input
+          type="text"
+          value="${inputElValue}"
+          class="text-xl w-[500px] mr-[52px] py-2 rounded-2xl"
+        />
+        <button 
+          class="categoryUpdateBtn w-[98px] h-[38px] bg-white rounded-[50px] border border-black mr-4 hover:border-red hover:text-red"
+        >
+          수정
+        </button>
+        <button 
+          class="categoryDelBtn w-[98px] h-[38px] bg-white rounded-[50px] border border-black hover:border-red hover:text-red"
+        >
+          삭제
+        </button>
+      </li>`;
+      categoryList.innerHTML += categoryLi;
+
+      const newCategoryLi = categoryList.lastElementChild;
+      updateCategory(newCategoryLi);
+      deleteCategory(newCategoryLi);
+
       alert("카테고리가 추가되었습니다.");
 
-      categoryName.value = "";
+      inputEl.value = "";
     }
-  });
-
-  categoryUpdateBtn.addEventListener("click", (e) => {
-    e.preventDefault();
-
-    confirm("카테고리를 수정하시겠습니까?");
   });
 }
