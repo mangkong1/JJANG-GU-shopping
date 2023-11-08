e70514aa45729a8e0af9e0530dab62edbb95c135
[SW7] 김태형 <buzzkim53@gmail.com>
2023-11-09 05:18:15 +0900

    for API data

--- pages/register/register.js
+++ pages/register/register.js
@@ -27,3 +27,37 @@ inputInfo.addEventListener("change", (e) => {
 
   reader.readAsDataURL(file);
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const registerBtn = document.querySelector(".registerBtn");
+
+  registerBtn.addEventListener("click", function () {
+    const productName = document.querySelector(".itemName").value; // 상품 이름 입력 필드 값 가져오기
+    const productPrice = parseInt(document.querySelector(".itemPrice").value); // 가격 입력 필드 값 가져오기
+    const stock = parseInt(document.querySelector(".itemStock").value); // 재고 입력 필드 값 가져오기
+    const description = document.querySelector(".itemDes").value; // 설명 입력 필드 값 가져오기
+
+    const data = {
+      "categories._id": 111,
+      name: productName,
+      price: productPrice,
+      stock: stock,
+      description: description,
+      images: [showItem, showInfo],
+    };
+
+    fetch("https://localhost:5000/products", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => {
+        alert("제품이 성공적으로 등록되었습니다!");
+      })
+      .catch((error) => {
+        alert("제품 등록 중 오류가 발생했습니다.");
+      });
+  });
+});
--- pages/sell/sell.js
+++ pages/sell/sell.js
@@ -1,104 +1,219 @@
-fetch("../product.json")
-  .then((response) => response.json())
-  .then((data) => {
-    const buyList = document.querySelector(".buyList");
+document.addEventListener("DOMContentLoaded", function () {
+  fetch("https://localhost:5000/products/{_id}", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      const buyList = document.querySelector(".buyList");
 
-    data.forEach((item) => {
-      const newItem = document.createElement("div");
-      newItem.classList.add("py-[20px]", "flex", "items-center");
-      newItem.innerHTML = `
-        <img
-          class="mx-[40px] rounded-[15px] w-[115px] h-[115px]"
-          src="/imgs/sss.jpeg"
-          alt="사진"
-        />
-        <p class="w-[150px] mx-[3px]">
-          ${item.name}
-          <br />
-          ${item.category}
-        </p>
-        <p class="mx-[150px]">${item.price}</p>
-        <button
-          type="submit"
-          class="modifyBtn mx-[15px] w-[98px] h-[38px] bg-white rounded-[50px] border border-black"
-        >
-          수정
-        </button>
-        <button
-          class="deleteBtn mx-[15px] w-[98px] h-[38px] bg-white rounded-[50px] border border-black"
-        >
-          삭제
-        </button>
-      `;
-      buyList.appendChild(newItem);
+      data.forEach((item) => {
+        const newItem = document.createElement("div");
+        newItem.classList.add("py-[20px]", "flex", "items-center");
+        newItem.innerHTML = `
+              <img
+                class="mx-[40px] rounded-[15px] w-[115px] h-[115px]"
+                src="/imgs/sss.jpeg"
+                alt="사진"
+              />
+              <p class="w-[150px] mx-[3px]">
+                ${item.name}
+                <br />
+                ${item.category}
+              </p>
+              <p class="mx-[150px]">${item.price}</p>
+              <button
+                type="submit"
+                class="modifyBtn mx-[15px] w-[98px] h-[38px] bg-white rounded-[50px] border border-black"
+              >
+                수정
+              </button>
+              <button
+                class="deleteBtn mx-[15px] w-[98px] h-[38px] bg-white rounded-[50px] border border-black"
+              >
+                삭제
+              </button>
+            `;
+        buyList.appendChild(newItem);
 
-      const deleteBtn = newItem.querySelector(".deleteBtn");
-      deleteBtn.addEventListener("click", () => {
-        buyList.removeChild(newItem);
-        data.items = data.items.filter((i) => i.num !== item.num);
-      });
+        const deleteBtn = newItem.querySelector(".deleteBtn");
+        deleteBtn.addEventListener("click", () => {
+          buyList.removeChild(newItem);
+          data.items = data.items.filter((i) => i.num !== item.num);
+        });
 
-      const modifyCheckPopup = document.querySelector("#modifyCheckPopup");
-      const popupBg = document.querySelector(".popupBg");
+        const modifyCheckPopup = document.querySelector("#modifyCheckPopup");
+        const popupBg = document.querySelector(".popupBg");
 
-      const modifyBtns = document.querySelectorAll(".modifyBtn");
-      modifyBtns.forEach((modifyBtn) => {
-        modifyBtn.addEventListener("click", (e) => {
-          e.preventDefault();
+        const modifyBtns = document.querySelectorAll(".modifyBtn");
+        modifyBtns.forEach((modifyBtn) => {
+          modifyBtn.addEventListener("click", (e) => {
+            e.preventDefault();
 
-          modifyCheckPopup.classList.remove("hidden");
-          popupBg.classList.remove("hidden");
-          window.scroll({ top: 0, behavior: "smooth" });
+            modifyCheckPopup.classList.remove("hidden");
+            popupBg.classList.remove("hidden");
+            window.scroll({ top: 0, behavior: "smooth" });
+          });
+        });
+
+        const inputItem = document.getElementById("inputItem");
+        const showItem = document.getElementById("showItem");
+
+        inputItem.addEventListener("change", (e) => {
+          const file = e.target.files[0];
+          const reader = new FileReader();
+
+          reader.onload = (e) => {
+            showItem.src = e.target.result;
+          };
+
+          reader.readAsDataURL(file);
+        });
+
+        const inputInfo = document.getElementById("inputInfo");
+        const showInfo = document.getElementById("showInfo");
+
+        inputInfo.addEventListener("change", (e) => {
+          const file = e.target.files[0];
+          const reader = new FileReader();
+
+          reader.onload = (e) => {
+            showInfo.src = e.target.result;
+          };
+
+          reader.readAsDataURL(file);
+        });
+
+        const popupBtnBack = document.querySelector(
+          ".popupBtns button:first-child"
+        );
+        const popupBtnChange = document.querySelector(
+          ".popupBtns button:last-child"
+        );
+
+        function hiddenPopup() {
+          modifyCheckPopup.classList.add("hidden");
+          popupBg.classList.add("hidden");
+        }
+
+        popupBtnBack.addEventListener("click", () => {
+          hiddenPopup();
+        });
+
+        popupBtnChange.addEventListener("click", () => {
+          hiddenPopup();
+          alert("수정이 완료되었습니다!");
         });
       });
-
-      const inputItem = document.getElementById("inputItem");
-      const showItem = document.getElementById("showItem");
-
-      inputItem.addEventListener("change", (e) => {
-        const file = e.target.files[0];
-        const reader = new FileReader();
-
-        reader.onload = (e) => {
-          showItem.src = e.target.result;
-        };
-
-        reader.readAsDataURL(file);
-      });
-
-      const inputInfo = document.getElementById("inputInfo");
-      const showInfo = document.getElementById("showInfo");
-
-      inputInfo.addEventListener("change", (e) => {
-        const file = e.target.files[0];
-        const reader = new FileReader();
-
-        reader.onload = (e) => {
-          showInfo.src = e.target.result;
-        };
-
-        reader.readAsDataURL(file);
-      });
-
-      const popupBtnBack = document.querySelector(
-        ".popupBtns button:first-child"
-      );
-      const popupBtnChange = document.querySelector(
-        ".popupBtns button:last-child"
-      );
-
-      function hiddenPopup() {
-        modifyCheckPopup.classList.add("hidden");
-        popupBg.classList.add("hidden");
-      }
-
-      popupBtnBack.addEventListener("click", () => {
-        hiddenPopup();
-      });
-
-      popupBtnChange.addEventListener("click", () => {
-        hiddenPopup();
-        alert("수정이 완료되었습니다!");
-      });
+    })
+    .catch((error) => {
+      alert("상품 조회 중 오류가 발생했습니다.");
     });
-  });
+});
+
+// fetch("../product.json")
+//   .then((response) => response.json())
+//   .then((data) => {
+//     const buyList = document.querySelector(".buyList");
+
+//     data.forEach((item) => {
+//       const newItem = document.createElement("div");
+//       newItem.classList.add("py-[20px]", "flex", "items-center");
+//       newItem.innerHTML = `
+//         <img
+//           class="mx-[40px] rounded-[15px] w-[115px] h-[115px]"
+//           src="/imgs/sss.jpeg"
+//           alt="사진"
+//         />
+//         <p class="w-[150px] mx-[3px]">
+//           ${item.name}
+//           <br />
+//           ${item.category}
+//         </p>
+//         <p class="mx-[150px]">${item.price}</p>
+//         <button
+//           type="submit"
+//           class="modifyBtn mx-[15px] w-[98px] h-[38px] bg-white rounded-[50px] border border-black"
+//         >
+//           수정
+//         </button>
+//         <button
+//           class="deleteBtn mx-[15px] w-[98px] h-[38px] bg-white rounded-[50px] border border-black"
+//         >
+//           삭제
+//         </button>
+//       `;
+//       buyList.appendChild(newItem);
+
+//       const deleteBtn = newItem.querySelector(".deleteBtn");
+//       deleteBtn.addEventListener("click", () => {
+//         buyList.removeChild(newItem);
+//         data.items = data.items.filter((i) => i.num !== item.num);
+//       });
+
+//       const modifyCheckPopup = document.querySelector("#modifyCheckPopup");
+//       const popupBg = document.querySelector(".popupBg");
+
+//       const modifyBtns = document.querySelectorAll(".modifyBtn");
+//       modifyBtns.forEach((modifyBtn) => {
+//         modifyBtn.addEventListener("click", (e) => {
+//           e.preventDefault();
+
+//           modifyCheckPopup.classList.remove("hidden");
+//           popupBg.classList.remove("hidden");
+//           window.scroll({ top: 0, behavior: "smooth" });
+//         });
+//       });
+
+//       const inputItem = document.getElementById("inputItem");
+//       const showItem = document.getElementById("showItem");
+
+//       inputItem.addEventListener("change", (e) => {
+//         const file = e.target.files[0];
+//         const reader = new FileReader();
+
+//         reader.onload = (e) => {
+//           showItem.src = e.target.result;
+//         };
+
+//         reader.readAsDataURL(file);
+//       });
+
+//       const inputInfo = document.getElementById("inputInfo");
+//       const showInfo = document.getElementById("showInfo");
+
+//       inputInfo.addEventListener("change", (e) => {
+//         const file = e.target.files[0];
+//         const reader = new FileReader();
+
+//         reader.onload = (e) => {
+//           showInfo.src = e.target.result;
+//         };
+
+//         reader.readAsDataURL(file);
+//       });
+
+//       const popupBtnBack = document.querySelector(
+//         ".popupBtns button:first-child"
+//       );
+//       const popupBtnChange = document.querySelector(
+//         ".popupBtns button:last-child"
+//       );
+
+//       function hiddenPopup() {
+//         modifyCheckPopup.classList.add("hidden");
+//         popupBg.classList.add("hidden");
+//       }
+
+//       popupBtnBack.addEventListener("click", () => {
+//         hiddenPopup();
+//       });
+
+//       popupBtnChange.addEventListener("click", () => {
+//         hiddenPopup();
+//         alert("수정이 완료되었습니다!");
+//       });
+//     });
+//   });
