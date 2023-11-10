fb42cbba9a7fd2229e25274541eb6235abdb87e1
[SW7] 박소영 <soyo19585@gmail.com>
2023-11-10 20:41:36 +0900

    sync with mainUi branch

--- pages/event.js
+++ pages/event.js
@@ -2,9 +2,6 @@ import "../style.css";
 import "../index.css";
 import { addItem } from "./cart/localStorage.js";
 
-const adminToken =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2NTRkMGIxOWQ5NDExN2E1ZTJlMzk3YTQiLCJyb2xlIjoiYWRtaW4iLCJpYXQiOjE2OTk1NTA2NTJ9.td4t4QMCj8U3A923THtanJLEfBLSbrggONfdKjOnE-w";
-
 // 배너 슬라이드
 function initSlide(time) {
   let currentIndex = 0;
@@ -41,21 +38,19 @@ function initSlide(time) {
     }
   }, time);
 }
-
 initSlide(3000);
 
 // 카테고리
 const categoryTabList = document.querySelector(".categoryTabList");
 const createCategoryTab = (category) => {
-  const categoryId = category["_id"];
   return `<li
   class="categoryTab inline-block border border-gray400 rounded-full mr-3 mb-3 text-gray400 overflow-hidden"
   >
-    <a href="/" class="w-full block py-1.5 px-7" id="${categoryId}">${category.name}</a>
+    <a class="w-full block py-1.5 px-7 cursor-pointer" id="${category._id}">${category.name}</a>
   </li>`;
 };
 
-// 카테고리 JSON으로 가져오기
+// 카테고리 조회
 fetch("http://kdt-sw-7-team03.elicecoding.com/api/categories", {
   method: "GET",
 })
@@ -63,7 +58,7 @@ fetch("http://kdt-sw-7-team03.elicecoding.com/api/categories", {
     if (!res.ok) {
       console.error(res.status, res.statusText);
     }
-    res.json();
+    return res.json();
   })
   .then((categoryList) => {
     // 카테고리 categoryTabList에 넣기
@@ -80,12 +75,11 @@ fetch("http://kdt-sw-7-team03.elicecoding.com/api/categories", {
 const productList = document.querySelector(".productList");
 const createProduct = (item) => {
   const productId = item["categories._id"];
-
   return `<li class="productItem">
   <div
     class="productItemThumb h-[360px] w-full rounded-2xl overflow-hidden relative"
   >
-    <i
+    <i id="${productId}"
       data-id="${item._id}"
       class="addCart fa-solid fa-cart-shopping text-2xl text-red w-14 h-14 bg-white90 flex justify-center items-center rounded-full hidden cursor-pointer block absolute left-[50%] bottom-[40px] translate-x-[-50%]"
     ></i>
@@ -109,11 +103,15 @@ const createProduct = (item) => {
 };
 
 // 제품 JSON으로 가져오기
-fetch("product.json")
+fetch("http://kdt-sw-7-team03.elicecoding.com/api/products", {
+  method: "GET",
+})
   .then((res) => {
+    if (!res.ok) {
+      console.error(res.status, res.statusText);
+    }
     return res.json();
   })
-  // 제품 productList에 넣기
   .then((productLiList) => {
     productLiList.forEach((product) => {
       const productLi = createProduct(product);
@@ -122,7 +120,6 @@ fetch("product.json")
 
     return productLiList;
   })
-  // 카테고리별로 해당 제품 보이게
   .then((productLiList) => {
     const categoryLi = document.querySelectorAll(".categoryTab");
 
@@ -141,6 +138,7 @@ fetch("product.json")
         category.classList.add("clicked");
         category.children[0].classList.add("clicked");
 
+        productList.textContent = "";
         const searchByCategoryProduct = [];
         const clickedCategoryTab = e.target.id;
 
@@ -149,8 +147,8 @@ fetch("product.json")
           searchByCategoryProduct.push(...productLiList);
         } else {
           productLiList.forEach((product) => {
-            const productId = product["categories._id"];
-            if (productId === clickedCategoryTab) {
+            const productCategoryId = product["category"][0]["_id"];
+            if (productCategoryId === clickedCategoryTab) {
               searchByCategoryProduct.push(product);
             }
           });
@@ -160,9 +158,7 @@ fetch("product.json")
         if (searchByCategoryProduct.length === 0) {
           productList.innerHTML = `<div class="text-center text-gray400 absolute left-2/4 translate-x-[-50%] ">상품이 없습니다.<div>`;
         } else {
-          const newProduct = searchByCategoryProduct
-            .map((product) => createProduct(product))
-            .join("");
+          const newProduct = searchByCategoryProduct.map((product) => createProduct(product)).join("");
           productList.innerHTML = newProduct;
         }
 
@@ -184,16 +180,14 @@ function showCartIcon() {
 
     viewCartEl.addEventListener("click", (e) => {
       addItem(e.target.dataset.id);
-      let isconfirm = confirm(
-        "아이템이 장바구니에 담겼습니다. 확인해보시겠습니까?"
-      );
+      let isconfirm = confirm("아이템이 장바구니에 담겼습니다. 확인해보시겠습니까?");
       if (isconfirm) {
         window.location.href = "/cart/";
       }
     });
 
     targetEl.addEventListener("click", (e) => {
-      localStorage.setItem("idTemp", e.target.dataset.id);
+      sessionStorage.setItem("idTemp", e.target.dataset.id);
     });
 
     targetEl.addEventListener("mouseover", () => {
