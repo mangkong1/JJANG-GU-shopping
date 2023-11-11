6237af66e695531f508a035960d53d1d23b379fc
choonghyun <lollibop@nate.com>
2023-11-11 23:31:12 +0900

    최종

--- pages/login/login.js
+++ pages/login/login.js
@@ -1,6 +1,3 @@
-
-
-// .-------------------------------------------아래가 최종버전//
 import '../../index.css';
 
 const loginId = document.getElementById('loginId');
@@ -27,8 +24,9 @@ loginBtn.addEventListener('click', async () => {
       // 로그인 성공, 여기서 토큰을 저장하거나 다른 동작을 수행할 수 있습니다.
       console.log('로그인 성공:', data);
       alert("로그인에 성공하셨습니다!")
+      window.location.href = '../index.html';
 sessionStorage.setItem("data", JSON.stringify(data));
-    //   window.location.href = '../index.html';
+      
     } else {
       // 로그인 실패
       console.error('로그인 실패:', data.error);
@@ -38,22 +36,4 @@ sessionStorage.setItem("data", JSON.stringify(data));
     console.error('오류 발생:', error);
   }
 });
-
-
-//------------------색깔 기능--------------
-// function color() {
-//     if ((loginId.value.length>4 && loginId.value.indexOf("@")!==4) 
-//              && loginPw.value.length>=8) {
-//         loginBtn.style.backgroundColor = "#F13737";
-//         loginBtn.disabled = false;
-//     } else {
-//         loginBtn.style.backgroundColor = "#F8C9C9";
-//         loginBtn.disabled = true;
-//     }
-// }
-
-
-// loginId.addEventListener('keyup', color);
-// loginPw.addEventListener('keyup', color);
-
 
