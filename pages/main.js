c0e08923bd1b8436adc99bf7e2de1dcc528b3325
[SW7] 박소영 <soyo19585@gmail.com>
2023-11-12 22:51:02 +0900

    ✨Feat: header change

--- pages/main.js
+++ pages/main.js
@@ -14,12 +14,12 @@ document.querySelector("#headerWrap").innerHTML = `
         <img class="h-full" src="../imgs/logo.svg" alt="떡잎마을 상점 로고" />
       </a></h1>
       <ul class="login">
-        <li class="inline-block px-4"><a href="/login/">로그인</a></li>
-        <li class="inline-block px-4"><a href="/sign/">회원가입</a></li>
-        <li class="inline-block px-4 hidden"><a href="#">로그아웃</a></li>
-        <li class="inline-block px-4 hidden"><a href="#">관리자</a></li>
-        <li class="inline-block px-4 relative hidden">
-          <a href="/userInfo/">
+        <li id="loginMenu" class="inline-block px-4"><a href="/login/">로그인</a></li>
+        <li id="signinMenu" class="inline-block px-4"><a href="/sign/">회원가입</a></li>
+        <li id="logoutMenu" class="inline-block px-4 hidden"><a href="/">로그아웃</a></li>
+        <li id="adminMenu" class="inline-block px-4 hidden"><a href="/admin-info/">관리자</a></li>
+        <li id="myPageMenu" class="inline-block px-4 relative hidden">
+          <a href="/user-info/">
             <i class="fa-solid fa-user text-lg"></i>
           </a>
         </li>
@@ -59,4 +59,51 @@ const scrollTopBtn = document.querySelector(".Scroll_top");
 
 scrollTopBtn.addEventListener("click", () => {
   window.scroll({ top: 0, behavior: "smooth" });
+});
+
+const token = JSON.parse(sessionStorage.getItem("data")).token;
+// const token = "a";
+
+const loginMenu = document.getElementById("loginMenu");
+const logoutMenu = document.getElementById("logoutMenu");
+const signinMenu = document.getElementById("signinMenu");
+const adminMenu = document.getElementById("adminMenu");
+const myPageMenu = document.getElementById("myPageMenu");
+
+if (sessionStorage.getItem("data") !== null) {
+  loginMenu.classList.add("hidden");
+  signinMenu.classList.add("hidden");
+
+  //토큰값을 보낼 수 있는 방법이 없어서
+  // 관리자만 접근 가능한 api인 카테고리에 삭제 접근 성공하면 (1234는 없는 카테고리) 관리자 메뉴가 뜨고
+  // 아니면 안뜨는 식으로 해뒀습니다.
+  fetch("http://kdt-sw-7-team03.elicecoding.com/api/categories/1234", {
+    method: "DELETE",
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  }).then((res) => {
+    if (res.status === 403) {
+      console.log("일반 유저입니다.");
+      logoutMenu.classList.remove("hidden");
+      myPageMenu.classList.remove("hidden");
+    } else if (!res.ok) {
+      console.error(res.status, res.statusText);
+    }
+    if (res.status === 400) {
+      //없는 카테고리이기때문에 bad request가 뜰것
+      adminMenu.classList.remove("hidden");
+      logoutMenu.classList.remove("hidden");
+    }
+  });
+}
+
+//로그아웃
+logoutMenu.addEventListener("click", (e) => {
+  sessionStorage.removeItem("data");
+  loginMenu.classList.remove("hidden");
+  signinMenu.classList.remove("hidden");
+  logoutMenu.classList.add("hidden");
+  adminMenu.classList.add("hidden");
+  myPageMenu.classList.add("hidden");
 });
